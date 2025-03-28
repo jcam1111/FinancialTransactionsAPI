e1c135c26031d9c9835718da85365c7389e7c167
userCamiloDaza <antirbd_11@hotmail.com>
2025-03-28 18:49:45 -0500

    Patron singleton acceso MongoDB

--- test.infrastructure/Settings/MongoDBSettings.cs
+++ test.infrastructure/Settings/MongoDBSettings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,39 @@ namespace test.infrastructure.Settings
 {
     public class MongoDBSettings
     {
-        public string ConnectionString { get; set; }
+        private static MongoDBSettings _instance;
+        //public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
+        public string ConnectionString { get; private set; }
+        private MongoDBSettings(IConfiguration configuration) {
+            ConnectionString = configuration.GetValue<string>("MongoDbSettings:ConnectionString");
+        }
+
+        //public static MongoDBSettings GetInstance()
+        //{
+        //    //if (_instance == null)
+        //    //{
+        //    //    _instance = new MongoDbSettings();
+        //    //    _instance.ConnectionString = "mongodb://localhost:27017"; // Ejemplo
+        //    //}
+        //    if (_instance == null)
+        //    {
+        //        // Si la instancia no existe, la creamos
+        //        _instance = new MongoDBSettings();
+        //        // Cargamos la configuración desde la fuente proporcionada (appsettings.json, variables de entorno, etc.)
+        //        _instance.ConnectionString = configuration.GetValue<string>("MongoDbSettings:ConnectionString");
+        //    }
+
+        //    return _instance;
+        //}
+        public static MongoDBSettings GetInstance(IConfiguration configuration)
+        {
+            if (_instance == null)
+            {
+                _instance = new MongoDBSettings(configuration);  // Creamos la instancia y pasamos la configuración
+            }
+
+            return _instance;
+        }
     }
 }
